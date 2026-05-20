4115ca7a105574f86411e46fe2648e1296a36420
Luis Miguel <luis.hernandezbermeo@estudiantesunibague.edu.co>
2026-05-20 00:29:44 -0500

    agrega canal noticias

--- src/observer_practice/canal.py
+++ src/observer_practice/canal.py
@@ -11,13 +11,4 @@ class CanalNoticias:
     def desuscribir(self, observador):
         if observador in self.observadores:
             self.observadores.remove(observador)
-
-    def notificar(self, mensaje):
-        for observador in self.observadores:
-            observador.actualizar(mensaje)
-
-    def publicar(self, mensaje):
-        self.ultimo_mensaje = mensaje
-        self.notificar(mensaje)
-
-#canal
+            
